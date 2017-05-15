0e34a8d40382a0b939730128bb258e88f072e848
Tobias Viehmann <tobias.viehmann@mni.thm.de>
2017-05-15 14:18:37 +0200

    Processing comments

--- src/main/scala/parser/WsAPI.scala
+++ src/main/scala/parser/WsAPI.scala
@@ -5,9 +5,15 @@ object WsAPI extends fastparse.WhitespaceApi.Wrapper(Lexical.ws)
 object Lexical {
   import fastparse.all._
 
-  val ws = P((wsChars|newline).rep)
-
   val wsChars = P(CharIn("\u0020\u0009"))
   val newline = P(StringIn("\r\n", "\n", "\r"))
 
+  val lineComment = P(("//" | "#") ~ CharsWhile(c => c != '\n' && c != '\r') ~ (newline | End))
+
+  val multilineText = P(CharsWhile(c => c != '*') | (!"*/" ~ AnyChar))
+  val multiLineComment = P("/*" ~ multilineText.rep ~ ("*/" | End))
+
+  val comment = P(lineComment | multiLineComment)
+
+  val ws = P(NoTrace((wsChars | newline | comment).rep))
 }
