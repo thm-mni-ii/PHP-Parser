19f3ef91122df169b64151af0d0582de5aea08d5
Tobias Viehmann <tobias.viehmann@mni.thm.de>
2017-07-06 16:43:13 +0200

    Improving parse results

--- src/main/scala/parser/PHPParser.scala
+++ src/main/scala/parser/PHPParser.scala
@@ -1,7 +1,40 @@
 package parser
 
-import parser.Basic.Script
+import ast.Basic.Script
+import fastparse.all._
+import fastparse.core.Frame
 
 object PHPParser {
-  def parse(toParse: String) = Script.parse(toParse)
+
+  def parse(toParse: String) = {
+    val result = parser.Basic.Script.parse(toParse)
+    result match {
+      case Parsed.Success(value, currentIndex) => {
+        PHPParser.Success(value)
+      }
+      case Parsed.Failure(parser, index, extra) => {
+        val msg =
+          s"""
+             |Parsing failed on ${extra.input.repr.prettyIndex(extra.input, index)}
+             |  expected: ${extra.traced.expected}
+             |  got: "...${extra.input.slice(index, index + 20)}"
+          """.stripMargin
+        val framePrint = extra.traced.stack.reverse.map { case Frame(index, p) => s"  while trying to parse '${p}' at ${extra.input.repr.prettyIndex(extra.input, index)}\n" }.mkString
+        val fullMsg =
+          s"""
+             |${msg}
+             |
+             |Parsing failed on "...${extra.input.slice(index, index + 20)}"
+             |  while trying to parse '${extra.traced.expected}' at ${extra.input.repr.prettyIndex(extra.input, index)}
+             |${framePrint}
+          """
+            .stripMargin
+        PHPParser.Failure(msg, fullMsg, result.asInstanceOf[Parsed.Failure])
+      }
+    }
+  }
+
+  sealed abstract class Result {}
+  case class Success(script: Script) extends PHPParser.Result
+  case class Failure(msg: String, fullMsg: String, failure: Parsed.Failure) extends PHPParser.Result
 }
